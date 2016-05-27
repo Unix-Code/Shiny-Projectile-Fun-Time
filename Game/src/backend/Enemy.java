15b8874f43fde26059ffa702e067c8759976385c
Unix-Code <dmalakh@comcast.net>
2016-05-27 12:36:11 -0400

    Enemies shoot projectiles
    Projectiles are shot radially upon pursuing a player

--- Game/src/backend/Enemy.java
+++ Game/src/backend/Enemy.java
@@ -4,6 +4,9 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Rectangle;
+import java.awt.geom.AffineTransform;
+import java.awt.image.AffineTransformOp;
+import java.awt.image.BufferedImage;
 import java.util.Date;
 
 /**
@@ -18,23 +21,37 @@ public class Enemy extends Character {
     
     private Rectangle perimeter;
     // in seconds
-    private final int followTime = 3;
+    private final int followTime = 3;    
     
     // For Disappearing
     private long startTime;
     private long thisTime;
     private boolean returning;
     private boolean starting = true;
-            
-    public Enemy(int x, int y, int w, int h, int health, int dmg, Handler handler) {
+    
+    public final int fireSpeed = 3;
+    private long shootTime;
+    private long currTime;
+    
+    private int currHealth;
+    private int prevHealth;
+    
+    public Enemy(int x, int y, int w, int h, int health, int dmg, /*BufferedImage img, */Handler handler) {
         super(x, y, w, h, 0, 0, health, ID.Enemy, handler);
         startX = x;
         startY = y;
         this.dmg = dmg;
-        perimeter = new Rectangle(x - 50, y - 50, this.w + 100, this.h + 100);
+        
+        perimeter = new Rectangle(x - 500, y - 500, this.w + 1000, this.h + 1000);
         startTime = (new Date().getTime());
         thisTime =  startTime + followTime*1000 + 1;
         returning = false;
+        
+        currTime = (new Date().getTime());
+        shootTime = currTime;
+        
+        currHealth = health;
+        prevHealth = health;
     }
     
     public void render(Graphics g) {
@@ -46,8 +63,63 @@ public class Enemy extends Character {
     
     public void tick() {
         super.tick();
-
+        
+        currTime = (new Date().getTime());
+        if (Math.abs(currTime - shootTime) > 1500 && startX != this.x && startY != this.y ) fire();
         this.watchForPlayer();
+    }
+    
+    public void fire() {
+        double theta = 0;
+        boolean secondTime = false;
+        while (Math.abs(theta) <=  Math.toRadians(180)) {      
+            // System.out.println(theta);
+            ProjectileType[] projectiles = {ProjectileType.Blast/*, ProjectileType.Strike/*, ProjectileType.Bolt*/};
+            
+            
+            // System.out.println(theta);
+            double dx =  8.0 * Math.cos(theta);
+            double dy = 8.0 * Math.sin(theta);
+            // System.out.println("dx: " + dx + ", dy: " + dy + "\nX: " + tempObject.getX() + ", Y: " + tempObject.getY() + "\nMouseX: " + mouse.getX() + ", MouseY: " + mouse.getY() + "\nCamX: " + cam.getX() + ", CamY: " + cam.getY());
+            double ratioX = (this.getW()/2)/(Math.abs(dx));
+            double ratioY = (this.getH()/2)/(Math.abs(dy));
+
+            double spawnX;
+            double spawnY;
+
+            if (ratioX < ratioY) {
+                spawnX = dx * ratioX * 1.2 + this.getVelX();
+                spawnY = dy * ratioX * 1.2 + this.getVelY();
+            }
+            else {
+                spawnX = dx * ratioY * 1.2 + this.getVelX();
+                spawnY = dy * ratioY * 1.2 + this.getVelY();
+            }
+
+            double adjustment = 1;
+
+            ProjectileType randProjectile = projectiles[(int) (Math.random() * (projectiles.length))];
+
+            BufferedImage projectileImg = randProjectile.getImages()[(int) (Math.random() * 4)];
+
+            // rotate projectile image
+            AffineTransform transform = new AffineTransform();
+            transform.rotate(theta, projectileImg.getWidth() / 2, projectileImg.getHeight() / 2);
+            AffineTransformOp op = new AffineTransformOp(transform, AffineTransformOp.TYPE_BICUBIC);
+            projectileImg = op.filter(projectileImg, null);
+
+            // if (dx/((Movable)tempObject).getVelX() > 0 && dy/((Movable)tempObject).getVelY() > 0) adjustment = 3;
+            // System.out.println("Adjustment: " + adjustment);
+            handler.addObject(new Projectile((int)(this.getX() + this.getW()/2.0 + spawnX), (int)(this.getY() + this.getH()/2.0 + spawnY), randProjectile.getSizeX(), randProjectile.getSizeY(), dx, dy, 0.3 * adjustment, 5, ID.EnemyProjectile, projectileImg , handler));
+            shootTime = (new Date().getTime());
+            
+            if (Math.abs(theta - Math.toRadians(180)) < 0.0001 && !secondTime) {
+                theta *= -1;
+                secondTime = true;
+            }
+            
+            theta += Math.toRadians(60);
+        }
     }
     
     public int getDamage() {
@@ -55,7 +127,7 @@ public class Enemy extends Character {
     }
     
     public void watchForPlayer() {
-        perimeter = new Rectangle(x - 50, y - 50, this.w + 100, this.h + 100);
+        perimeter = new Rectangle(x - 500, y - 500, this.w + 1000, this.h + 1000);
         
         boolean inPursuit = thisTime - startTime < (followTime * 1000);
         
